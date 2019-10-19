4f1e94be9a2f7404a343e222b397f7d0c2a740c2
Carlos Betancur <cbetanca@jci.com>
2019-10-19 11:48:22 -0500

    Welcome Service Checkin

---
+++ pisco-mall/src/app/welcome/welcome.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WelcomeService {
+  url = 'https://masrekognition.firebaseio.com/';
+  constructor(private http: HttpClient) { }
+
+  getUser(id) {
+    return this.http.get(`${this.url}users.json?equalTo=${id}&orderBy="userId"`);
+  }
+}

28a807a7688d7c877451339df4a1ef067a1a88bc
Luis Mejia <mejialuis28@gmail.com>
2019-10-18 19:07:57 -0500

    Rekognition api for comparison.

--- PiscoMarketApi/PiscoMarketApi/Controllers/RekognitionController.cs
+++ PiscoMarketApi/PiscoMarketApi/Controllers/RekognitionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PiscoMarketApi.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,17 +12,23 @@ namespace PiscoMarketApi.Controllers
     [Route("api/[controller]")]
     public class RekognitionController : Controller
     {
+        private readonly IRekognitionService _rekognitionService;
+        public RekognitionController(IRekognitionService rekognition)
+        {
+            _rekognitionService = rekognition;
+        }
+
         // POST api/<controller>
         // Validate New FAce
         [HttpPost]
-        public string Post([FromBody]dynamic imagesArray)
+        public async Task<string> Post([FromBody]dynamic imagesArray)
         {
             //Get first image string.
             if(imagesArray!=null)
             {
                 string base64String = imagesArray[0];
-                //Return ID
-                return "1";
+                byte[] bytes = Convert.FromBase64String(base64String.Replace("data:image/png;base64,", ""));
+                return await _rekognitionService.Compare(bytes);
             }
             return "not recognized";
         }
---
+++ PiscoMarketApi/PiscoMarketApi/Services/RekognitionService.cs
@@ -0,0 +1,49 @@
+﻿using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PiscoMarketApi.Services
+{
+    public class RekognitionService : IRekognitionService
+    {
+        public async Task<string> Compare(byte[] imageBytes)
+        {
+            try
+            {
+                String collectionId = "PiscoMarketFaces";
+                var rekognitionClient = new AmazonRekognitionClient();
+                var image = new Image()
+                {
+                    Bytes = new MemoryStream(imageBytes)
+                };
+
+                var searchFacesByImageRequest = new SearchFacesByImageRequest()
+                {
+                    CollectionId = collectionId,
+                    Image = image,
+                    FaceMatchThreshold = 70F,
+                    MaxFaces = 1
+                };
+
+                SearchFacesByImageResponse searchFacesByImageResponse =
+                    await rekognitionClient.SearchFacesByImageAsync(searchFacesByImageRequest);
+
+                if (searchFacesByImageResponse.FaceMatches.Any())
+                {
+                    var faceMatch = searchFacesByImageResponse.FaceMatches.First();
+                    return faceMatch.Face.ExternalImageId;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                
+                return null;
+            }
+            
+        }
+    }
+}

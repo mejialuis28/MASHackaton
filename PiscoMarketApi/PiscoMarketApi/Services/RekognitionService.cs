a7c7b04e88b10f30e9b6a9ab1caffd19cbb2cfe1
Carlos Betancur <cbetanca@jci.com>
2019-10-19 09:09:14 -0500

    Deployment Adjustments

--- PiscoMarketApi/PiscoMarketApi/Services/RekognitionService.cs
+++ PiscoMarketApi/PiscoMarketApi/Services/RekognitionService.cs
@@ -1,20 +1,36 @@
-﻿using Amazon.Rekognition;
+﻿using Amazon;
+using Amazon.Rekognition;
 using Amazon.Rekognition.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using PiscoMarketApi.Models;
 using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace PiscoMarketApi.Services
 {
     public class RekognitionService : IRekognitionService
     {
+        private AwsConfiguration configuration;
+
+        public RekognitionService(IOptions<AwsConfiguration> awsOptions)
+        {
+            configuration = awsOptions.Value;
+        }
+
         public async Task<string> Compare(byte[] imageBytes)
         {
             try
             {
                 String collectionId = "PiscoMarketFaces";
-                var rekognitionClient = new AmazonRekognitionClient();
+                              
+                var rekognitionClient = new AmazonRekognitionClient(configuration.awsAccessKey, configuration.awsAccessSecret, Amazon.RegionEndpoint.USEast2);
+
                 var image = new Image()
                 {
                     Bytes = new MemoryStream(imageBytes)
@@ -27,7 +43,7 @@ namespace PiscoMarketApi.Services
                     FaceMatchThreshold = 70F,
                     MaxFaces = 1
                 };
-
+                
                 SearchFacesByImageResponse searchFacesByImageResponse =
                     await rekognitionClient.SearchFacesByImageAsync(searchFacesByImageRequest);
 
@@ -36,14 +52,15 @@ namespace PiscoMarketApi.Services
                     var faceMatch = searchFacesByImageResponse.FaceMatches.First();
                     return faceMatch.Face.ExternalImageId;
                 }
+
                 return null;
             }
             catch (Exception ex)
             {
-                
+
                 return null;
             }
-            
+
         }
     }
 }
